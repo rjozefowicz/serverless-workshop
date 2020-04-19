6b2fad30ea3a6690afb4ea593e8b4a358623db85
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2020-04-19 23:07:25 +0200

    my-notes-frontend initial

---
+++ my-notes-frontend/precache-manifest.747eacbf161fa9889b1c004c74e78af7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6970bd757b475fd6ddc9c13fb38e255b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11aec85367cd2b0b3c1e",
+    "url": "/static/css/main.ffc592a4.chunk.css"
+  },
+  {
+    "revision": "c0e0ef9a4b9e0a57649a",
+    "url": "/static/js/2.2e43ff83.chunk.js"
+  },
+  {
+    "revision": "11aec85367cd2b0b3c1e",
+    "url": "/static/js/main.b22776bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
