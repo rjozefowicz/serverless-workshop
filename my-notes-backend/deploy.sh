969ee36b56a5bc72f92406ae837659c94d5edc01
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2020-04-20 23:22:24 +0200

    variables to custom and adding S3 bucket for attachments configuration

---
+++ my-notes-backend/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+npm install --production
+
+cd functions/create-note
+npm install --production
+cd ../..
+
+cd functions/list-notes
+npm install --production
+cd ../..
+
+cd functions/delete-note
+npm install --production
+cd ../..
+
+cd functions/file-upload
+npm install --production
+cd ../..
+
+cd functions/list-notes
+npm install --production
+cd ../..
+
+sls deploy --stage radek
