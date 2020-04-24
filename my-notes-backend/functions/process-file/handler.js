f5861d79adc4a723babf239ceb8290f26f1a0a4a
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2020-04-24 22:46:04 +0200

    process-file

--- my-notes-backend/functions/process-file/handler.js
+++ my-notes-backend/functions/process-file/handler.js
@@ -1,6 +1,27 @@
 'use strict';
 
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
 module.exports.func = async(event) => {
+
+    for (let record of event.Records) {
+        const key = formURLDecodeComponent(record.s3.object.key);
+        const idFileName = key.split("/"); /* S3 location: userId/noteId/fileName  */
+        const note = {
+            userId: idFileName[0],
+            noteId: idFileName[1],
+            title: idFileName[2],
+            timestamp: new Date().getTime(),
+            s3Location: key,
+            size: record.s3.object.size,
+            type: getType(idFileName[2])
+        };
+        await documentClient.put({
+            TableName: process.env.TABLE_NAME,
+            Item: note
+        }).promise();
+    }
 
 };
 
@@ -12,4 +33,8 @@ module.exports.func = async(event) => {
 
 function formURLDecodeComponent(stringToDecode) {
     return decodeURIComponent((stringToDecode + '').replace(/\+/g, ' '));
+}
+
+function getType(fileName) {
+    return (/\.(jpg|jpeg|png)$/i).test(fileName) ? "IMAGE" : "FILE";
 }
