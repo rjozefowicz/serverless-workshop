4af961198fc260b32d28a0c19ce5123d2e2376f1
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2020-04-24 23:16:51 +0200

    delete-note-s3

--- my-notes-backend/functions/delete-note/handler.js
+++ my-notes-backend/functions/delete-note/handler.js
@@ -1,18 +1,28 @@
 const AWS = require('aws-sdk');
-
+const S3 = new AWS.S3({
+    signatureVersion: 'v4'
+});
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.func = async (event, context) => {
 
     const userId = event.requestContext.authorizer.claims["cognito:username"];
 
-    await documentClient.delete({
+    const deletedNote = await documentClient.delete({
         TableName: process.env.TABLE_NAME,
         Key: {
             userId,
             noteId: event.pathParameters.id
-        }
+        },
+        ReturnValues: 'ALL_OLD'
     }).promise();
+
+    if (deletedNote.Attributes.s3Location !== undefined) {
+        await S3.deleteObject({
+            Bucket: process.env.BUCKET_NAME,
+            Key: deletedNote.Attributes.s3Location
+        }).promise();
+    }
 
     return response(200);
 };
