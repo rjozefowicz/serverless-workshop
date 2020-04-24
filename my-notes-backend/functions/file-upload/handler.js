e9d63dfa27b60d2b5a7a4e9a173a626e33d71b53
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2020-04-24 22:20:52 +0200

    file-upload

--- my-notes-backend/functions/file-upload/handler.js
+++ my-notes-backend/functions/file-upload/handler.js
@@ -1,7 +1,26 @@
 'use strict';
 
+const AWS = require("aws-sdk");
+const S3 = new AWS.S3({
+    signatureVersion: 'v4'
+});
+const uuid = require("uuid/v4");
+
 module.exports.func = async(event, context) => {
-    return response(200);
+
+    const uploadRequest = JSON.parse(event.body);
+    const userId = event.requestContext.authorizer.claims["cognito:username"];
+    const location = `${userId}/${uuid()}/${uploadRequest.name}`;
+
+    const signedUrl = S3.getSignedUrl("putObject", {
+        Bucket: process.env.BUCKET_NAME,
+        Key: location,
+        Expires: 100
+    });
+
+    return response(200, {
+        link: signedUrl
+    });
 };
 
 function response(statusCode, body) {
