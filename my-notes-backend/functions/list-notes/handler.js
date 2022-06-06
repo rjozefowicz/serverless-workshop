d2b57a5c1210e80c8355bd36b7a5d54ea6c28190
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2022-06-06 16:10:45 +0200

    zeto - list notes

--- my-notes-backend/functions/list-notes/handler.js
+++ my-notes-backend/functions/list-notes/handler.js
@@ -1,7 +1,21 @@
 'use strict';
 
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
 module.exports.func = async (event, context) => {
-    return response(200);
+
+    const notes = await documentClient.query({
+        TableName: process.env.TABLE_NAME,
+        KeyConditionExpression: "userId = :userId",
+        ExpressionAttributeValues: {
+            ":userId": "23"
+        }
+    }).promise();
+
+    return response(200, {
+        elements: notes.Items
+    });
 };
 
 function response(statusCode, body) {
