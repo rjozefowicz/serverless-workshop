ef5ac47aecc2b3b52b8ef54fe8b3455f5b2f9c48
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2022-06-06 15:24:56 +0200

    zeto - create note

--- my-notes-backend/functions/create-note/handler.js
+++ my-notes-backend/functions/create-note/handler.js
@@ -1,6 +1,22 @@
 'use strict';
 
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
 module.exports.func = async(event, context) => {
+
+    const note = JSON.parse(event.body);
+    note.userId = "23";
+    note.noteId = v4();
+    note.timestamp = new Date().getTime();
+    note.type = "TEXT";
+
+    await documentClient.put({
+        Item: note,
+        TableName: process.env.TABLE_NAME
+    }).promise();
+
     return response(200);
 };
 
